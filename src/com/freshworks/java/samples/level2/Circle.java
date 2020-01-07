6f272a95a8a5deaf8522719268b9189bf58df63d
geethalladi <geeth.alladi@freshworks.com>
2020-01-07 12:54:31 +0530

    Circle Figure

---
+++ src/com/freshworks/java/samples/level2/Circle.java
@@ -0,0 +1,14 @@
+package com.freshworks.java.samples.level2;
+
+public class Circle extends Figure {
+    final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * (radius * radius);
+    }
+}
