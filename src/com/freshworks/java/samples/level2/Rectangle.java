7169325515965973a3ef7885d4c33ce758a0b23f
geethalladi <geeth.alladi@freshworks.com>
2020-01-07 12:54:54 +0530

    Rectangle Figure

---
+++ src/com/freshworks/java/samples/level2/Rectangle.java
@@ -0,0 +1,16 @@
+package com.freshworks.java.samples.level2;
+
+class Rectangle extends Figure {
+    final double length;
+    final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width  = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
