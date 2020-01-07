29d7daa399f84ca539d9b10e098154513f378b89
geethalladi <geeth.alladi@freshworks.com>
2020-01-07 13:58:16 +0530

    Creating own exception Class

---
+++ src/com/freshworks/java/samples/level3/EmptyStackException.java
@@ -0,0 +1,9 @@
+package com.freshworks.java.samples.level3;
+
+public class EmptyStackException extends IllegalStateException {
+
+    /**
+     * Serialization Identifier
+     */
+    private static final long serialVersionUID = -8047517380801195723L;
+}
