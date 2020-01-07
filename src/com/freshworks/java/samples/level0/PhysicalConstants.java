46618b73109be6414b77bbf3f181398faf0dbf24
geethalladi <geeth.alladi@freshworks.com>
2020-01-07 12:41:43 +0530

    Containers for storing constants

---
+++ src/com/freshworks/java/samples/level0/PhysicalConstants.java
@@ -0,0 +1,16 @@
+package com.freshworks.java.samples.level0;
+
+// Constant utility class
+public class PhysicalConstants
+{
+  private PhysicalConstants() { }  // Prevents instantiation
+
+  // Avogadro's number (1/mol)
+  public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+  // Boltzmann constant (J/K)
+  public static final double BOLTZMANN_CONST  = 1.380_648_52e-23;
+
+  // Mass of the electron (kg)
+  public static final double ELECTRON_MASS    = 9.109_383_56e-31;
+}
